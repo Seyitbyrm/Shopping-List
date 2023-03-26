a268d6d7ee2364eea5d6fba8ce5caca76ea7162d
Seyitbyrm <seyitbyrm@gmail.com>
2023-03-26 20:26:25 +0300

    26.03.2023

--- script.js
+++ script.js
@@ -4,37 +4,14 @@ var input = document.querySelector("#userinput");
 var ul = document.querySelector("ul");
 var dlt = document.querySelector("#kaldir");
 
-function inputLength() {
-    return input.value.length;
-}
-
-function createListElement() {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
-}
-
-function addListAfterClick() {
-    if(input.value.length > 0) {
-        createListElement();
-    } 
-}
-
-function addListAfterKeypress() {
-    if(input.value.length > 0 && event.code === "Enter") {
-        createListElement();
-    }
-}
-
-function deleteListElement(){
-    ul.removeChild(ul.lastChild);
-}
+let inputLength = () => input.value.length;
+let createListElement = () => {var li = document.createElement("li"); li.appendChild(document.createTextNode(input.value)); ul.appendChild(li); input.value = ``;}
+let addListAfterClick = () => input.value.length > 0 ? createListElement():``;
+let addListAfterKeypress = () => input.value.length > 0 && event.code === "Enter" ? createListElement() : ``;
+let deleteListElement = () => ul.removeChild(ul.lastChild) ;
 
 button.addEventListener("click", addListAfterClick);
-
 input.addEventListener("keypress", addListAfterKeypress);
-
 dlt.addEventListener("click",deleteListElement);
 
 

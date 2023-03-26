b62fdff577dfbccec3245409e1f692ad103665bc
Seyitbyrm <112037984+Seyitbyrm@users.noreply.github.com>
2023-03-26 21:52:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var ul = document.querySelector("ul");
 var dlt = document.querySelector("#kaldir");
 
 let inputLength = () => input.value.length;
-let createListElement = () => {var li = document.createElement("li"); li.appendChild(document.createTextNode(input.value)); ul.appendChild(li); input.value = ``;}
+let createListElement = () => {let li = document.createElement("li"); li.appendChild(document.createTextNode(input.value)); ul.appendChild(li); input.value = ``;}
 let addListAfterClick = () => input.value.length > 0 ? createListElement():``;
 let addListAfterKeypress = () => input.value.length > 0 && event.code === "Enter" ? createListElement() : ``;
 let deleteListElement = () => ul.removeChild(ul.lastChild) ;

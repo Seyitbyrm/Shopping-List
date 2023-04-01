59ce1c16b660fe1ca92bd841bbaac0e7f7fd4247
Seyitbyrm <112037984+Seyitbyrm@users.noreply.github.com>
2023-04-01 10:19:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-var button = document.querySelector("#enter");
-var input = document.querySelector("#userinput");
-var ul = document.querySelector("ul");
-var dlt = document.querySelector("#kaldir");
+const button = document.querySelector("#enter");
+const input = document.querySelector("#userinput");
+const ul = document.querySelector("ul");
+const dlt = document.querySelector("#kaldir");
 
-let inputLength = () => input.value.length;
-let createListElement = () => {let li = document.createElement("li"); li.appendChild(document.createTextNode(input.value)); ul.appendChild(li); input.value = ``;}
-let addListAfterClick = () => input.value.length > 0 ? createListElement():``;
-let addListAfterKeypress = () => input.value.length > 0 && event.code === "Enter" ? createListElement() : ``;
-let deleteListElement = () => ul.removeChild(ul.lastChild) ;
+const inputLength = () => input.value.length;
+const createListElement = () => {let li = document.createElement("li"); li.appendChild(document.createTextNode(input.value)); ul.appendChild(li); input.value = ``;}
+const addListAfterClick = () => input.value.length > 0 ? createListElement():``;
+const addListAfterKeypress = () => input.value.length > 0 && event.code === "Enter" ? createListElement() : ``;
+const deleteListElement = () => ul.removeChild(ul.lastChild) ;
 
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
